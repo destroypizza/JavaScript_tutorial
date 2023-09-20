1af3a5d26ed99dadfef921c396826a13187de6a9
prokopii <pizzadestroy@gmail.com>
2023-09-21 00:43:46 +0900

    test2

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-console.log("Hello, World!!!");
-console.log("Hello, \n World!");
+//console.log("Hello, World!!!");
+//console.log("Hello, \n World!");
 
-a = 1;
-b = 2;
-c = a + b
+// a = 1;
+// b = 2;
+
+// a = 5;
+// c = a + b
+//console.log(c)
+
+a = "abcd"
+d = 4
+
+b = "efgh"
+f = 2
+
+c = a + b + d + f
+
 console.log(c)
 
---
+++ test2.js
@@ -0,0 +1,11 @@
+//2-nd test
+a = "abcd";
+b = "efgh";
+
+c = 2;
+d = 7;
+
+//We want watch this answer ---> "abcdefgh 6"
+
+answer = `${a+b} ${c+d}`;
+console.log(answer);
